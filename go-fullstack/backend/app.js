8c966387496826792bcb594c8322e5633fcfa35e
Spunkikitty <travelbeanie@gmail.com>
2021-10-13 17:38:40 -0400

    Upload A

--- go-fullstack/backend/app.js
+++ go-fullstack/backend/app.js
@@ -18,8 +18,13 @@ process.on('unhandledRejection', (reason, promise) => {
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require ('mongoose');
+const path = require('path');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
+
+
 
 const app = express();
 
@@ -49,6 +54,9 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', sauceRoutes)
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
   
 module.exports = app;
